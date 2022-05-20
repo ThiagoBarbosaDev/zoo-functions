49df1fee4b4f4d45482f284575fc36af17fce60c
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-20 14:55:27 -0300

    exercicio 2 completo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,6 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
+  const bicho = species.find((especie) => especie.name === animal);
+  return bicho.residents.every((individuo) => individuo.age >= age);
   // seu código aqui
 }
 
