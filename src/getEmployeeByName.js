c09b99fd83a67689b1996aaa6360d4b57f4c94b6
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-20 14:56:43 -0300

    exercicio 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const empregado = employees.find((employee) => employee.firstName === employeeName
+  || employee.lastName === employeeName);
+  if (empregado) { return empregado; }
+  return {};
 }
 
 module.exports = getEmployeeByName;
