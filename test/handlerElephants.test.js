6c534808397d9005ae6b9fe01b60887b78dabd14
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-21 16:59:10 -0300

    linting

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -9,15 +9,15 @@ it('Verifica se retorna uma string com o erro "Parâmetro inválido(...)" quando
   const error = 'Parâmetro inválido, é necessário uma string';
   expect(handlerElephants(9)).toBe(error);
   expect(handlerElephants([9])).toBe(error);
-  expect(handlerElephants({a: 9})).toBe(error);
+  expect(handlerElephants({ a: 9 })).toBe(error);
   expect(handlerElephants(handlerElephants)).toBe(error);
   expect(handlerElephants('hamburger')).toBe(null);
 });
-it('Verifica se retorna undefined se receber undefined como parametro', () => {
+it('Verifica se retorna os parâmetros válidos padrões', () => {
   expect(handlerElephants('count')).toBe(4);
   expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
   expect(handlerElephants('averageAge')).toBe(10.5);
-})
-it('Verifica se retorna undefined se receber undefined como parametro', () => {
+});
+it('Verifica se retorna os parâmetros válido excepcionais', () => {
   expect(handlerElephants('id')).toBe('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5');
-})
+});
