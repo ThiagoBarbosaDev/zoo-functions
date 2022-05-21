c8e4818714f40be986010b1a8c8b713d88163ca6
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-21 18:09:56 -0300

    formatting

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,6 +1,6 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
 it('Verifica se getElephants existe e é uma função', () =>
   expect(typeof handlerElephants).toBe('function'));
 it('Verifica se retorna undefined se receber undefined como parametro', () =>
@@ -20,4 +20,5 @@ it('Verifica se retorna os parâmetros válidos padrões', () => {
 });
 it('Verifica se retorna os parâmetros válido excepcionais', () => {
   expect(handlerElephants('id')).toBe('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5');
+});
 });
