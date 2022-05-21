79136925e3d11e4c825b9c8ca1b931478bb45516
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-21 18:27:08 -0300

    85% opening hours

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,26 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Testa se é uma função', () => {
+    expect(typeof getOpeningHours).toBe('function');
+  });
+  it('Testa se ao não passa nenhum parâmetro retorna todos os horários', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+  it('Testa se retorna que o zoológico está fechado nos horário apropriados', () => {
+    expect(getOpeningHours('Monday', '2:23-PM')).toBe('The zoo is closed');
+    expect(getOpeningHours('Tuesday', '2:23-AM')).toBe('The zoo is closed');
+  });
+  it('Testa se retorna que o zoológico está aberto nos horário apropriados', () => {
+    expect(getOpeningHours('Wednesday', '2:23-PM')).toBe('The zoo is open');
+    expect(getOpeningHours('Tuesday', '8:23-AM')).toBe('The zoo is open');
+  });
+});
