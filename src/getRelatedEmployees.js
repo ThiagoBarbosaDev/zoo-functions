6c4fdd6807f7f6e1501c38f02286e4bd0bd9e051
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-25 12:11:16 -0300

    style: remoção dos consolelogs

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,28 +1,9 @@
 const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-// let managerList = [];
-// employees
-//   .forEach((person) => managerList.push(person.managers))
-//   .reduce((acc, curVal) => acc.concat(curVal));
-
-// function isManager(id) {
-//   // seu código aqui
-//   const managerStatus = employees.find(
-//     (employee) =>
-//       employee.id === managerList.find((managerEntry) => managerEntry === id),
-//   );
-//   if (managerStatus) {
-//     return true;
-//   }
-//   return false;
-// }
 const isManager = (id) => employees
   .some((employee) => employee.managers
     .find((employeeid) => employeeid === id) === id);
-
-console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
