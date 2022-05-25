9098212df3d919ddfa4d7ab0c5b1004d40732c60
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-25 10:34:55 -0300

    reformatting requirement 3: converted into arrow function; general formatting; polished validation logic

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,12 +1,11 @@
 const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-  const empregado = employees.find((employee) => employee.firstName === employeeName
-  || employee.lastName === employeeName);
-  if (empregado) { return empregado; }
-  return {};
-}
+const getEmployeeByName = (employeeName = 0) => {
+  if (!employeeName) { return {}; }
+  const empregado = employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  return empregado;
+};
 
 module.exports = getEmployeeByName;
