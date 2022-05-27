f13bd9e61115c3266c7b7e53142a85c8dc0bf2a0
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-27 20:57:39 -0300

    refactor: req10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,18 +2,13 @@ const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const findSpecies = (animalId) => species.find((specie) => specie.id === animalId);
-// console.log(findSpecies('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-  const animalList = employees.find((person) => person.id === id).responsibleFor;
-  const arrayOfAnimalObjects = animalList.map(findSpecies).map((unit) => unit.residents);
-  const oldestAnimalObj = arrayOfAnimalObjects[0].reduce((acc, curVal) =>
-    (curVal.age > acc.age ? curVal : acc));
-  const oldestAnimalArray = [oldestAnimalObj.name, oldestAnimalObj.sex, oldestAnimalObj.age];
-  return oldestAnimalArray;
-}
+const getOldestFromFirstSpecies = (id) => {
+  const oldestAnimal = employees
+    .find((person) => person.id === id).responsibleFor
+    .map(findSpecies)[0].residents
+    .sort((a, b) => b.age - a.age)[0];
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+};
 
-// console.log(getOldestFromFirstSpecies("c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1"))
-//  Expected: ["Maxwell", "male", 15]
 module.exports = getOldestFromFirstSpecies;
